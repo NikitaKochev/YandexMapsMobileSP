e3f85dc9a10e55ffb1552960af8869e3f987edfe
NSKochev <nikitakochev.iosdev@gmail.com>
2023-11-29 10:49:48 +0300

    [Target] Added remote artifact to package

--- Package.swift
+++ Package.swift
@@ -4,5 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "YandexMapsMobileSP"
+    name: "YandexMapsMobileSP",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "YandexMapsMobileSP",
+            targets: ["YandexMapsMobile"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.4.0-full.framework.zip",
+            checksum: "e5ccf772a1d196c5f1ac4bdb353a4aae4183933183dd7207ded4ba851c92dbb5"
+        ),
+    ]
 )
